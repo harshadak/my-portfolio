0f9dcba484ea6a21cbb6e6f6e79f629d5b311fd2
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2016-01-31 15:05:41 -0800

    Made some design changes. 1. Changed "View works" to "View my works". 2. Reduced or got rid of the delay on certain components.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
-(function($){
-    $.fn.scrollingTo = function( opts ) {
+(function ($) {
+    $.fn.scrollingTo = function (opts) {
         var defaults = {
-            animationTime : 1000,
-            easing : '',
-            callbackBeforeTransition : function(){},
-            callbackAfterTransition : function(){}
+            animationTime: 1000,
+            easing: '',
+            callbackBeforeTransition: function () {},
+            callbackAfterTransition: function () {}
         };
 
-        var config = $.extend( {}, defaults, opts );
+        var config = $.extend({}, defaults, opts);
 
-        $(this).click(function(e){
+        $(this).click(function (e) {
             var eventVal = e;
             e.preventDefault();
 
-            var $section = $(document).find( $(this).data('section') );
-            if ( $section.length < 1 ) {
+            var $section = $(document).find($(this).data('section'));
+            if ($section.length < 1) {
                 return false;
             };
 
-            if ( $('html, body').is(':animated') ) {
-                $('html, body').stop( true, true );
+            if ($('html, body').is(':animated')) {
+                $('html, body').stop(true, true);
             };
 
             var scrollPos = $section.offset().top;
 
-            if ( $(window).scrollTop() == scrollPos ) {
+            if ($(window).scrollTop() == scrollPos) {
                 return false;
             };
 
             config.callbackBeforeTransition(eventVal, $section);
 
             $('html, body').animate({
-                'scrollTop' : (scrollPos+'px' )
-            }, config.animationTime, config.easing, function(){
+                'scrollTop': (scrollPos + 'px')
+            }, config.animationTime, config.easing, function () {
                 config.callbackAfterTransition(eventVal, $section);
             });
         });
@@ -41,40 +41,40 @@
 
 
 
-jQuery(document).ready(function(){
-	"use strict";
-	new WOW().init();
+jQuery(document).ready(function () {
+    "use strict";
+    new WOW().init();
 
 
-(function(){
- jQuery('.smooth-scroll').scrollingTo();
-}());
+    (function () {
+        jQuery('.smooth-scroll').scrollingTo();
+    }());
 
 });
 
 
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
 
 
 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 50) {
-            $(".navbar-brand a").css("color","#fff");
+            $(".navbar-brand a").css("color", "#fff");
             $("#top-bar").removeClass("animated-header");
         } else {
-            $(".navbar-brand a").css("color","inherit");
+            $(".navbar-brand a").css("color", "inherit");
             $("#top-bar").addClass("animated-header");
         }
     });
 
     $("#clients-logo").owlCarousel({
- 
-        itemsCustom : false,
-        pagination : false,
-        items : 5,
+
+        itemsCustom: false,
+        pagination: false,
+        items: 5,
         autoplay: true,
 
     })
@@ -87,32 +87,21 @@ $(document).ready(function(){
 $(".fancybox").fancybox({
     padding: 0,
 
-    openEffect : 'elastic',
-    openSpeed  : 450,
+    openEffect: 'elastic',
+    openSpeed: 450,
 
-    closeEffect : 'elastic',
-    closeSpeed  : 350,
+    closeEffect: 'elastic',
+    closeSpeed: 350,
 
-    closeClick : true,
-    helpers : {
-        title : { 
-            type: 'inside' 
+    closeClick: true,
+    helpers: {
+        title: {
+            type: 'inside'
         },
-        overlay : {
-            css : {
-                'background' : 'rgba(0,0,0,0.8)'
+        overlay: {
+            css: {
+                'background': 'rgba(0,0,0,0.8)'
             }
         }
     }
 });
-
-
-
-
-
-
- 
-
-
-
-
